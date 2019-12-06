8027a9be3877d6dece1acf28f9d6ce86bd35d951
geekboysupreme <shuvam.manna@gmail.com>
2019-12-07 00:45:05 +0530

    First Build

---
+++ build/precache-manifest.ae257b635e5dd4f5bed622cb23026412.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f790d6359461ca60473995fea9d84a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bfda1f53ee906fc5afa",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "47311c6fb53c6f3daca2",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "4bfda1f53ee906fc5afa",
+    "url": "/static/js/2.8185fb6d.chunk.js"
+  },
+  {
+    "revision": "47311c6fb53c6f3daca2",
+    "url": "/static/js/main.9d8a7bd9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
